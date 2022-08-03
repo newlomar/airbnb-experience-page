a9cfcd61aa4b4cd8b0fd13ef16dcde0c3df70a67
Newton Lomar <newtonlomar16@gmail.com>
2022-08-03 08:47:21 -0300

    Hero component finished

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import heroImg from '../assets/hero.png';
 export default function Hero() {
   return (
     <section class='hero__container'>
-      <div>
+      <div className='hero__img-div'>
         <img className='hero__img' src={heroImg} alt='' />
       </div>
     </section>
