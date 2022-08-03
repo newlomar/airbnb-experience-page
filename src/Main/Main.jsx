6dc16d828ab4c11c0522821d048ec4373666751d
Newton Lomar <newtonlomar16@gmail.com>
2022-08-03 08:55:21 -0300

    started working on Main component and on its sub-component: Card

---
+++ src/Main/Main.jsx
@@ -0,0 +1,16 @@
+import Card from '../Card/Card';
+
+export default function Main() {
+  return (
+    <main>
+      <h1>Online Experiences</h1>
+      <p>
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+      <Card />
+      <Card />
+      <Card />
+    </main>
+  );
+}
