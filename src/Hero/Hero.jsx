46aa8853d80c94546e8ee4e8928276eb3bb235f0
Newton Lomar <newtonlomar16@gmail.com>
2022-08-04 08:46:11 -0300

    typo correction

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import heroImg from '../assets/hero.png';
 
 export default function Hero() {
   return (
-    <section class='hero__container'>
+    <section className='hero__container'>
       <div className='hero__img-div'>
         <img className='hero__img' src={heroImg} alt='' />
       </div>
